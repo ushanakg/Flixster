7f8389efc7c733ea4ffe4c578e3d77f0519d0fbf
Ushana Goyal <ushanakgoyal@gmail.com>
2020-06-24 10:00:51 -0700

    Initial commit

---
+++ app/src/main/java/com/example/flixster/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.flixster;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.codepath.asynchttpclient.AsyncHttpClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    //public static final String NOW_PLAYING_URL = " https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    //AsyncHttpClient client = new AsyncHttpClient();
+
+}
